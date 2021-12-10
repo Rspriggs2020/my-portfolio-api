8659d76ae976dc1555226bf8867fddefb2adf11c
ryliespriggs <rspriggs812@gmail.com>
2021-12-09 20:53:18 -0800

    commented out uneeded code in project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :update, :destroy]
+  #before_action :set_project, only: [:show, :update, :destroy]
 
   # GET /projects
   def index
@@ -14,38 +14,38 @@ class ProjectsController < ApplicationController
   end
 
   # POST /projects
-  def create
-    @project = Project.new(project_params)
+  #def create
+   # @project = Project.new(project_params)
 
-    if @project.save
-      render json: @project, status: :created, location: @project
-    else
-      render json: @project.errors, status: :unprocessable_entity
-    end
-  end
+    #if @project.save
+     # render json: @project, status: :created, location: @project
+    #else
+     # render json: @project.errors, status: :unprocessable_entity
+    #end
+  #end
 
   # PATCH/PUT /projects/1
-  def update
-    if @project.update(project_params)
-      render json: @project
-    else
-      render json: @project.errors, status: :unprocessable_entity
-    end
-  end
+  #def update
+   # if @project.update(project_params)
+    #  render json: @project
+    #else
+     # render json: @project.errors, status: :unprocessable_entity
+    #end
+  #end
 
   # DELETE /projects/1
-  def destroy
-    @project.destroy
-  end
+  #def destroy
+   # @project.destroy
+  #end
 
-  private
+  #private
     # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
+   # def set_project
+    #  @project = Project.find(params[:id])
+    #end
 
     # Only allow a list of trusted parameters through.
-    def project_params
-      params.require(:project).permit(:project_title)
-    end
+    #def project_params
+     # params.require(:project).permit(:project_title)
+    #end
 end
